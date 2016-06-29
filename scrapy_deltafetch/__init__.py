9594ab6aaf88a56a6da17a026534759bb32d3e0b
Paul Tremberth <paul.tremberth@gmail.com>
2016-06-29 22:21:18 +0200

    Bump version: 1.0.1 → 1.1.0

--- scrapy_deltafetch/__init__.py
+++ scrapy_deltafetch/__init__.py
@@ -1,4 +1,4 @@
 from .middleware import DeltaFetch
 
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scrapy-deltafetch',
-    version='1.0.1',
+    version='1.1.0',
     license='BSD',
     description='Scrapy middleware to ignore previously crawled pages',
     author='Scrapinghub',
