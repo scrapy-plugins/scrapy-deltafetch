aea3c34dfab4d9256f37dbc70e27068b11b78a7c
Daniel Graña <dangra@gmail.com>
2017-02-09 16:48:24 -0300

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scrapy-deltafetch',
-    version='1.2.0',
+    version='1.2.1',
     license='BSD',
     description='Scrapy middleware to ignore previously crawled pages',
     author='Scrapinghub',
