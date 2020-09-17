0945860be50e591257934a7d93b5a974765dd58b
rabinadk1 <rabin.adk1@gmail.com>
2020-09-17 15:22:09 +0545

    Added info and additional arguments in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+with open(
+    "README.rst",
+) as fh:
+    long_description = fh.read()
 
 setup(
     name="scrapy-deltafetch",
     version="1.2.1",
-    license="BSD",
     description="Scrapy middleware to ignore previously crawled pages",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     author="Scrapinghub",
     author_email="info@scrapinghub.com",
+    maintainer="Rabin Adhikari",
+    maintainer_email="rabin.adk1@gmail.com",
     url="http://github.com/scrapy-plugins/scrapy-deltafetch",
-    packages=["scrapy_deltafetch"],
-    platforms=["Any"],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    license="BSD",
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
@@ -21,4 +29,6 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     install_requires=["Scrapy>=2.3.0", "bsddb3"],
+    python_requires=">=3.6",
+    zip_safe=True,
 )
