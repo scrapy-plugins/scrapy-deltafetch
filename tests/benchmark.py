3352cbc7fbc149404868a39af49fa466ad4d6928
Adrián Chaves <adrian@chaves.io>
2025-02-25 21:40:07 +0100

    Use ruff

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,6 +1,6 @@
 import tempfile
+from unittest import mock
 
-import mock
 from scrapy import Request, Spider
 from scrapy.statscollectors import StatsCollector
 from scrapy.utils.test import get_crawler
@@ -9,7 +9,7 @@ from scrapy_deltafetch import DeltaFetch
 
 
 def benchmark_middleware(result):
-    spider_name = 'df_tests'
+    spider_name = "df_tests"
     spider = Spider(spider_name)
     temp_dir = tempfile.gettempdir()
     crawler = get_crawler(Spider)
@@ -17,15 +17,15 @@ def benchmark_middleware(result):
     mw = DeltaFetch(temp_dir, reset=False, stats=stats)
     mw.spider_opened(spider)
     response = mock.Mock()
-    response.request = Request('http://url',
-                               meta={'deltafetch_key': 'key'})
+    response.request = Request("http://url", meta={"deltafetch_key": "key"})
 
-    for x in mw.process_spider_output(response, result, spider):
+    for _x in mw.process_spider_output(response, result, spider):
         pass
+
 
 def test_middleware(benchmark):
     result = []
     for x in range(50000):
-        request = Request(f'https://{x}')
+        request = Request(f"https://{x}")
         result.append(request)
     result = benchmark(benchmark_middleware, result)
